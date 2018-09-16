bfba81fc43813d99b8fe088ce893110c408d5f20
Isaac Blandin Mac <myung0966@gmail.com>
2018-09-15 23:43:31 -0500

    added 2 ans 3 point buttons

--- scoringApp/app/src/main/java/com/example/isaacblandin/scoringapp/MainActivity.java
+++ scoringApp/app/src/main/java/com/example/isaacblandin/scoringapp/MainActivity.java
@@ -21,8 +21,28 @@ public class MainActivity extends AppCompatActivity {
         displayBlueScore(blueScore);
     }
 
+    public void blueTwoScore(View view){
+        blueScore += 2;
+        displayBlueScore(blueScore);
+    }
+
+    public void blueThreeScore(View view){
+        blueScore += 3;
+        displayBlueScore(blueScore);
+    }
+
     public void redOneScore(View view){
         redScore ++;
+        displayRedScore(redScore);
+    }
+
+    public void redTwoScore (View view){
+        redScore += 2;
+        displayRedScore(redScore);
+    }
+
+    public void redThreeScore(View view){
+        redScore += 3;
         displayRedScore(redScore);
     }
 
